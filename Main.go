73a0b327c7288c86636f9a67c9f9993a7beb1d1f
hgoncerz <hubertg489655@gmail.com>
2022-08-05 13:42:06 +0200

    variable int uint float32 and default floate64

--- Main.go
+++ Main.go
@@ -3,5 +3,41 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Hello");
+	// var nameOne string = "mario"
+	// var nameTwo = "luigi"
+	// var nameThree string
+
+
+	// fmt.Println(nameOne,nameTwo,nameThree)
+
+	// nameOne = "peach"
+	// nameThree = "bowser"
+	
+	// fmt.Println(nameOne,nameTwo,nameThree)
+
+	// nameFour := "yoshi"
+
+	// fmt.Println(nameFour)
+
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	//bit & memory
+
+	// var numOne int8 = 25
+	// var numTwo int8 = -128
+
+	// var numThree uint = 250
+
+	var scoreOne float32 = 25.98
+	var scoreTwo float64 = 12312398719827398123987719382.7
+	scoreThree := 1.5
+
+	fmt.Println(scoreOne, scoreTwo,scoreThree)
+
+
+
+
 }
